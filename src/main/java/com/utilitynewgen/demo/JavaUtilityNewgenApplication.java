56c110de8d2a61baf7a289c8e32ca5d29732df81
Anurag <anurag.sach28@gmail.com>
2020-04-22 16:49:43 +0530

    initial commit

---
+++ src/main/java/com/utilitynewgen/demo/JavaUtilityNewgenApplication.java
@@ -0,0 +1,22 @@
+package com.utilitynewgen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+
+@SpringBootApplication
+@EnableScheduling
+public class JavaUtilityNewgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUtilityNewgenApplication.class, args);
+		printthis();
+	}
+	@Scheduled(fixedRate = 1000)
+	static void printthis() {
+		System.out.println("I am printing this");
+	}
+
+}
---
+++ src/test/java/com/utilitynewgen/demo/JavaUtilityNewgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utilitynewgen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUtilityNewgenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
